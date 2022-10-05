eb21f9badfc641f137700b63aa81a2868af6ee3c
Samantha Guillemette <samantha.guillemette@gmail.com>
2022-10-05 15:12:36 -0400

    added help get method

--- LabAssignment1/Source/httpc.py
+++ LabAssignment1/Source/httpc.py
@@ -12,17 +12,23 @@ def help_method():
     return string
 
 
+def help_get_method():
+  string = '\nusage: httpc get [-v] [-h key:value] URL\n\nGet executes a HTTP GET request for a given URL.\n\t-v\t' + \
+               '\tPrints the detail of the response such as protocol, status, and headers.\n\t-h key:value\tAssociates ' \
+               'headers to HTTP Request with the format "key:value".'
+  return string
+
 
 
 parser = argparse.ArgumentParser(description='httpc is a curl-like application but supports HTTP protocol only.')
 parser.add_argument('command', type=str, help=help_method())
-parser.add_argument('-gh', '--gethelp', help='get', action="store_true")
+parser.add_argument('-gh', '--gethelp', help=help_get_method(), action="store_true")
 parser.add_argument('-ph', '--posthelp', help='post', action="store_true")
 args = parser.parse_args()
 
 if args.command == 'help':
     if args.gethelp:
-        print("get")
+        print(help_get_method())
     elif args.posthelp:
         print("post")
     else:
