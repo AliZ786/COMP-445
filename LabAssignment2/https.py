2d833f456d79638dd460ec5e661cadd5edd38c93
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-11-14 18:28:59 -0500

    Added help method

--- LabAssignment2/https.py
+++ LabAssignment2/https.py
@@ -6,5 +6,26 @@ import socket
 from server import Server
 from manager import Manager
 
-class HTTPS:
+response_string = ''
+
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("command", type=str, choices = ["?"])
+args = parser.parse_args()
+print(args)
+
+def get_help_response():
+  response_string = '\nhttps is a simple file server.\n' \
+                    'usage: httfs [-v] [-p PORT] [-d PATH-TO-DIR]\n' \
+                     '-v Print debugging messages.\n' \
+                     '-p Specifies the port number that the server will listen and serve at. Default is 8080.\n' \
+                     '-d Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.'
   
+  return response_string
+
+
+if args.command == "?":
+  print(get_help_response())
+
+
+
