79e3f8902dce5a8d96c5a6a8343a05fda383dee7
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-11-14 22:56:36 -0500

    Added server file

--- LabAssignment2/main.py
+++ LabAssignment2/main.py
@@ -13,6 +13,12 @@ data = ''
 header = ''
 request_string = ''
 request_response = ''
+content_type = ''
+http_version = '1.1'
+file = ''
+file_content = ''
+
+
 
 
 
@@ -97,4 +103,99 @@ def post_file(file, directory, content):
 
   return response_string
 
+
+def checkRequest(request):
+
   
+    if request == "getFiles":
+        print("Getting all files from the directory.....\n")
+    
+    elif request == "defaultGet":
+      print("Getting directory....\n")
+    
+    elif request == "defaultPost":
+      print("Posting to default directory....\n")
+
+    elif request == "postFile":
+        print("Posting content for file.....\n")
+
+    elif request == "getFile":
+        print("Getting specific file from the command.....\n")
+
+    elif request =="download":
+        print("Downloading content.....\n")
+    
+    else:
+      print("Request is invalid....\n")
+        
+
+    return request
+
+
+def splitRequest(request):
+  content_type = 'application/json'
+
+  header, request_response = request.split('\r\n\r\n')
+
+  header_arr = header.split('\r\n')
+
+  method, request_string, http_version - header_arr[0].split('')
+
+  for string in header_arr[1:]:
+    if re.match(r'Content-Type', string):
+      content_type = string.split(':')[1]
+    
+    elif re.match(r'Content-Disposition', string):
+      data = checkRequest('dowload')
+      if re.match(r'/(.+)', request_string):
+        file = request_string[1:]
+
+  callRequest()
+
+def callRequest():
+  if method == "GET":
+    if re.match(r'/get', request_string):
+      if request_string in ['/get', '/get?']:
+        temp_string = ''
+      
+      else:
+        temp_location = request_string.split('?')[-1]
+        characters = {}
+        for item in temp_location.split('&'):
+          key, value = item.split('=')
+          characters[key] = value
+        
+        temp_string = characters
+
+      data = checkRequest("defaultGet")
+    
+    elif request_string == '/':
+      data = checkRequest('getFiles')
+    
+    elif re.match(r'/(.+)', request_string):
+      data = checkRequest('getFile')
+      file = request_string[1:]
+      
+    else:
+      data = checkRequest('invalid')
+
+  elif method == "POST":
+    if request_string == '/post':
+       file_content = request_response
+       data = checkRequest('defaultPost')
+    
+    elif re.match(r'/(.+)', request_string):
+      data = checkRequest('postFile')
+      file_content = request_response
+      file = request_string[1:]
+
+  else:
+    file = checkRequest('invalid')
+
+  return data, file, file_content
+
+
+
+
+
+
